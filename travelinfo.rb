370d0bd14a399a9b8b268a8dd67fcc49c5bbf6f1
Taylor D Jones <72883059+TaylorDJones11@users.noreply.github.com>
2024-02-22 18:28:57 +0000

    adding instance methods

--- travelinfo.rb
+++ travelinfo.rb
@@ -21,9 +21,39 @@ class Itinerary
   def itinerary_list
     loop do
       puts "\n"
-      puts "Travel time!"
+      puts "Let's start building your itinerary"
+      puts "1. Add destination"
+      puts "2. Add depature date"
+      puts "3. Add return date"
+      puts "4. Add your activities"
+      puts "5. Finished building my itinerary"
+
+      choice = gets.chomp.to_i
+
+      case choice
+      when 1
+        add_destination
+      when 2
+        add_depature
+      when 3
+        add_return
+      when 4
+        add_activities
+      when 5
+      end
     end
   end
 
+  def add_destination
+  end
+
+  def add_depature
+  end
+
+  def add_return
+  end
+
+  def add_activities
+  end
 
 end
