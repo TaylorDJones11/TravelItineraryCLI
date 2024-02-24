bb6e31d4b86365e9ca1f6b90e34f782185cb0361
Taylor D Jones <72883059+TaylorDJones11@users.noreply.github.com>
2024-02-24 21:59:56 +0000

    adding test validations

--- tests/test_travelinfo.rb
+++ tests/test_travelinfo.rb
@@ -2,5 +2,32 @@ require 'minitest/autorun'
 require './travelinfo'
 
 class TestTravelInfo < Minitest::Test
-  
+  def setup
+    @itinerary = Itinerary.new
+  end
+
+  def test_add_destination
+    @itinerary.add_destination("New York")
+    assert_equal "New York", @itinerary.destination
+  end
+
+  def test_add_departure
+    @itinerary.add_departure("2024-03-01")
+    assert_equal Date.new(2024, 3,1), @itinerary.departure_date
+  end
+
+  def test_add_return
+    @itinerary.add_return("2024-03-05")
+    assert_equal Date.new(2024,3,5), @itinerary.return_date
+  end
+
+  def test_valid_date
+    assert @itinerary.valid_date?("2024-03-01")
+    refute @itinerary.valid_date?("2024-02-30")
+  end
+
+  def test_add_activities
+    @itinerary.add_activities("Sightseeing")
+    assert_equal "Sightseeing", @itinerary.activities
+  end
 end
